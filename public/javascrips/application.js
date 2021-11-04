d21a349e7d1ddf1686b65fa7f0ee96b9f5d917df
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2021-11-04 19:36:59 -0400

    update

--- public/javascrips/application.js
+++ public/javascrips/application.js
@@ -1,1 +1,12 @@
-console.log("hello world!");
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? Deleting a todo cannot be undone!")
+    if (ok) {
+      this.submit();
+    }
+  });
+});
